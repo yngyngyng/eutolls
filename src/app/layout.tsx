d97d77afdd6888de82cda1b23e1686da4441b7e8
yngyngyng <yng@yngdev.com>
2024-10-30 00:53:22 +0100

    cookie consent because we don't love tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next'
 import './globals.css'
+import 'vanilla-cookieconsent/dist/cookieconsent.css'
 import GoogleAnalytics from '@/lib/ga'
+import { CookieConsentComponent } from '@/components/ui/cookie-consent'
 
 export const metadata: Metadata = {
 	title: '🇪🇺 EU Tolls',
@@ -15,6 +17,7 @@ export default function RootLayout({
 	return (
 		<html lang='en' className='dark'>
 			<GoogleAnalytics />
+			<CookieConsentComponent />
 			<body className='w-full dark:bg-black/[0.98] bg-white dark:bg-grid-stone-50/[0.02] bg-grid-black/[0.2] relative flex items-center justify-center'>
 				{children}
 			</body>
